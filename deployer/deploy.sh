079c3a6c67e3bef09c5b61338ad9e687520ec2dd
Bohdan Sukhomlinov <shellshock1953@gmail.com>
2022-12-28 23:53:14 +0200

    NOTE TERMINATE

--- deployer/deploy.sh
+++ deployer/deploy.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 set -eu
+# DEPLOY_KEY=/app/.ssh/ssh.private
+WORKDIR=/app/clone
+
+_help() {
+  echo """
+  Deployer - change k8s manifest and commit changes
+
+  Args:
+  -f -- file to be changed and commited
+  -k -- (key) - yaml path in -f(ile) to be changed
+  -v -- (value) - new values to be set for -k(ey)
+
+  Envs:
+  DEPLOY_KEY - plain ssh key to be used to commit
+
+"""
+}
 
 # ./deploy.sh -f test/test.yaml -k .global.key -v latest
 while getopts ":f:k:v:" opt; do
   case $opt in
-    f) FILE="$OPTARG"
+    f) FILE=$(printf '%s\n' "${OPTARG//[[:space:]]}")
     ;;
-    k) YKEY="$OPTARG"
+    k) YKEY=$(printf '%s\n' "${OPTARG//[[:space:]]}")
     ;;
-    v) YVALUE="$OPTARG"
+    v) YVALUE=$(printf '%s\n' "${OPTARG//[[:space:]]}")
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
+    _help
     exit 1
     ;;
   esac
 
   case $OPTARG in
     -*) echo "Option $opt needs a valid argument"
+    _help
     exit 1
     ;;
   esac
@@ -24,7 +43,7 @@ done
 
 git_config() {
   eval $(ssh-agent -s)
-  # echo "${DEPLOY_KEY}" | tr -d '\r' | ssh-add - # TODO
+  echo "${DEPLOY_KEY}" | tr -d '\r' | ssh-add - # TODO
   mkdir -p ~/.ssh
   chmod 700 ~/.ssh
   ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
@@ -33,14 +52,27 @@ git_config() {
   git config --global user.email "deployer@example.com" # TODO
 }
 
+git_pull() {
+  git clone "${REPO}" "${WORKDIR}"
+  cd ${WORKDIR}
+  git checkout "${REVISION}"
+}
+
 file_update() {
-  yq --inplace ''${YKEY}'="'${YVALUE}'"' "${FILE}"
+  cd ${WORKDIR}
+  REPLACE=''"${YKEY}=\"${YVALUE}\""''
+  ls -la
+  yq --inplace ${REPLACE} "${FILE}"
 }
 
 git_commit() {
+  cd ${WORKDIR}
   git add ${FILE}
-  git commit -m "${FILE} - ${IMAGE}"
-  git push origin ${REV:-main}
+  git commit -m "${FILE}: ${YVALUE}"
+  git push origin "${REVISION}"
 }
 
+git_config
+git_pull
 file_update
+git_commit
